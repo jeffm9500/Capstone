6c199cc1e6c6c3d21038cb32338fd8f99c6c2b7d
Lukas Bauer <lukas.bauer.j@gmail.com>
2020-11-01 16:22:27 -0500

    Cleaned up and commented aruco scripts

--- PatternDetect/detectAruco.py
+++ PatternDetect/detectAruco.py
@@ -2,36 +2,29 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 
+"""
+Script uses cv2 built-in ArUco detection to detect an ArUco marker, draw a box around it, and print the ID of the marker in the console.
+
+"""
 
 cap = cv2.VideoCapture(0)
-
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    #print(frame.shape) #480x640
+    
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     parameters =  aruco.DetectorParameters_create()
 
-    #print(parameters)
-
-    '''    detectMarkers(...)
-        detectMarkers(image, dictionary[, corners[, ids[, parameters[, rejectedI
-        mgPoints]]]]) -> corners, ids, rejectedImgPoints
-        '''
-        #lists of ids and the corners beloning to each id
+    # Lists of ids and the corners beloning to each id
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-    #print(corners)
-    print("Marker in frame. Distort face.")
+    if ids is not None:
+        print(f'Detecting ArUco with ID:{ids}')
 
-    #It's working.
-    # my problem was that the cellphone put black all around it. The alrogithm
-    # depends very much upon finding rectangular black blobs
-
+    # Draw region around the ArUco box
     frame = aruco.drawDetectedMarkers(frame, corners)
 
-    #print(rejectedImgPoints)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- PatternDetect/generateAruco.py
+++ PatternDetect/generateAruco.py
@@ -2,18 +2,18 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 
+"""
+Script generates an ArUco marker and saves it under test_marker.jpg. The marker is defined from the 250 size aruco.DICT.
+"""
 
-'''
-    drawMarker(...)
-        drawMarker(dictionary, id, sidePixels[, img[, borderBits]]) -> img
-'''
+# Use this to specify which ID of marker to be genreated. Choose between 1-250.
+markerID = 2
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 print(aruco_dict)
 
-# second parameter is id number (if using 250 dictionary then choose between 1-250)
-# last parameter is total image size
-img = aruco.drawMarker(aruco_dict, 2, 700)
+# Final parameter in drawMarker is the total image size
+img = aruco.drawMarker(aruco_dict, markerID, 700)
 cv2.imwrite("test_marker.jpg", img)
 
 cv2.imshow('frame',img)
